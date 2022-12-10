00be386b1dd21342313f199a897c971e8f5e9623
Dominic Rutkowski <rutkowski.dominic@gmail.com>
2022-12-09 20:02:35 -0500

    Parse the .env file

--- index.js
+++ index.js
@@ -7,5 +7,10 @@ import * as nj from 'nunjucks';
 dotenv.config();
 
 (async () => {
-  console.log(`Hello, ${process.env.NAME}!`);
+  const name = process.env.NAME ? process.env.NAME.trim() : '';
+  const classes = process.env.CLASSES ? process.env.CLASSES.split(',').map(courseName => courseName.trim()) : [];
+  const collaborators = process.env.COLLABORATORS ? process.env.COLLABORATORS.split(',').map(collaboratorName => collaboratorName.trim()) : [];
+  console.log(`Hello, ${name}!`);
+  console.log(`You are enrolled in ${classes}.`);
+  console.log(`Your potential collaborators are ${collaborators}.`);
 })();
